fd23ec9d5c71fc5b97fd9cab6e0573c07d05a8e1
Gio <giovanni.berl1@gmail.com>
2020-09-09 11:52:49 +0200

    prove update pivot

--- app/Http/Controllers/Admin/PromotionController.php
+++ app/Http/Controllers/Admin/PromotionController.php
@@ -13,59 +13,65 @@ class PromotionController extends Controller
 {
 
 
-  
 
-  public function transaction(Flat $flat){
-    
-    $thisFlat=Flat::where('id', $flat->id)->first();
-    $promotions=Promotion::all();
+
+  public function transaction(Flat $flat)
+  {
+
+    $thisFlat = Flat::where('id', $flat->id)->first();
+    $promotions = Promotion::all();
 
     $gateway = makeGateway();
-    
-    $clientToken=$gateway->ClientToken()->generate(); 
-    $data=[
-      'promotions'=>$promotions,
-      'flat'=>$thisFlat,
-      'clientToken'=>$clientToken
+
+    $clientToken = $gateway->ClientToken()->generate();
+    $data = [
+      'promotions' => $promotions,
+      'flat' => $thisFlat,
+      'clientToken' => $clientToken
     ];
     return view('admin.transaction.payment', $data);
   }
 
-  public function process(Flat $flat, Request $request){
+  public function process(Flat $flat, Request $request)
+  {
     $payload = $request->payment_method_nonce;
     //$amount = $request->input('amount', false);
-    $promotion=Promotion::where('id', $request->promotion)->first();
-    $flat=Flat::where('id', $flat->id)->first();
-    $amount=$promotion->price;
-    
+    $promotion = Promotion::where('id', $request->promotion)->first();
+    $flat = Flat::where('id', $flat->id)->first();
+    $amount = $promotion->price;
+
     $gateway = makeGateway();
 
     $status = $gateway->transaction()->sale([
-	    'amount' => $amount,
-	    'paymentMethodNonce' => $payload,
-	    'options' => [
-	      'submitForSettlement' => True
-    	]
+      'amount' => $amount,
+      'paymentMethodNonce' => $payload,
+      'options' => [
+        'submitForSettlement' => True
+      ]
     ]);
-    
-    if($status->success || !is_null($status->transaction)){
-    
-    $flat->is_promoted= 1;
-    $flat->save();
-    $now = Carbon::now();
-    
-    $flat->promotions()->sync([$promotion->id]);
-    
-    foreach ($flat->promotions as $promotion) {
-       $promotion->pivot->created_at->update($now);
-  }
-    /* $flat->promotions->pivot->started_at->update($now); */
+
+    if ($status->success || !is_null($status->transaction)) {
+
+      $flat->is_promoted = 1;
+      $flat->save();
+      $now = Carbon::now();
+
+
+
+
+      // $pivot = Flat::find($flat->id)->promotions[$promotion->id]->pivot;
+      // $pivot->started_at = $now;
+      /* $flat->promotions->pivot->started_at->update($now); */
+      $flat->promotions()->updateExistingPivot($flat->id, ['started_at' => $now]);
+
+      $flat->promotions()->sync([$promotion->id]);
 
 
 
 
 
-      return redirect()->route('admin.flats.show', ['flat'=>$flat->id]);
+
+      return redirect()->route('admin.flats.show', ['flat' => $flat->id]);
     } else {
       return abort('404');
     }
--- database/seeds/PromotionSeeder.php
+++ database/seeds/PromotionSeeder.php
@@ -12,24 +12,22 @@ class PromotionSeeder extends Seeder
      */
     public function run()
     {
-        $promotionA=new Promotion();
-        $promotionA->type= 'base';
-        $promotionA->duration=24;
-        $promotionA->price=2.99;
+        $promotionA = new Promotion();
+        $promotionA->type = 'base';
+        $promotionA->duration = 1;
+        $promotionA->price = 2.99;
         $promotionA->save();
 
-        $promotionB=new Promotion();
-        $promotionB->type= 'medium';
-        $promotionB->duration=72;
-        $promotionB->price=5.99;
+        $promotionB = new Promotion();
+        $promotionB->type = 'medium';
+        $promotionB->duration = 3;
+        $promotionB->price = 5.99;
         $promotionB->save();
 
-        $promotionC=new Promotion();
-        $promotionC->type= 'premium';
-        $promotionC->duration=144;
-        $promotionC->price=9.99;
+        $promotionC = new Promotion();
+        $promotionC->type = 'premium';
+        $promotionC->duration = 6;
+        $promotionC->price = 9.99;
         $promotionC->save();
-
-
     }
 }
